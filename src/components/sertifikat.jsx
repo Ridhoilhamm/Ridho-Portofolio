b06d80ca661ece5dac17f29ce367942758e1bdb9
Ridhonya <110644274+Ridhoilhamm@users.noreply.github.com>
2025-03-24 15:58:47 +0700

    update sertifikat

--- src/components/sertifikat.jsx
+++ src/components/sertifikat.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 import { Carousel } from "./ui/caroucel";
 
 const slidesData = [
-    { src: "/images/data-analyst.png" },
-    { src: "/images/javascript.png" },
-    { src: "/images/cyber.png" },
-    { src: "/images/teaching-factory.png" },
+  { src: "/images/data-analyst.png" },
+  { src: "/images/javascript.png" },
+  { src: "/images/cyber.png" },
+  { src: "/images/teaching-factory.png" },
 ];
 
 const Sertifikat = () => {
-    return (
-        <div id="portofolio" className="container mx-auto max-w-[1200px] px-4 mt-20  mb-20 sm:px-6 lg:px-8">
-            <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Certificate</h2>
-            <div className=' flex justify-center items-center'>
-
-            <Carousel slides={slidesData} />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div
+      id="portofolio"
+      className="container mx-auto max-w-[1200px] px-4 mt-20 mb-20 sm:px-6 lg:px-8"
+    >
+      <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
+        Certificate
+      </h2>
+      <div className="flex justify-center items-center w-full overflow-hidden">
+        <Carousel slides={slidesData} className="w-full max-w-full" />
+      </div>
+    </div>
+  );
+};
 
 export default Sertifikat;
